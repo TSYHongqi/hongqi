8309c49320622c1ae8faa7df42d41a93ad42d9d1
TSYH <1642162589@qq.com>
2020-01-04 14:22:50 +0800

    Initial commit

---
+++ src/main/java/com/xr/hongqi/HongqiApplication.java
@@ -0,0 +1,13 @@
+package com.xr.hongqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongqiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongqiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xr/hongqi/controller/HelloController.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: HelloController
+ * Author:   TSYH
+ * Date:     2020-01-04 10:41
+ * Description:
+ * History:
+ * <author>     <time>       <version>     <desc>
+ * 作者姓名    修改时间     版本号       描述
+ */
+package com.xr.hongqi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author TSYH
+ * @create 2020-01-04
+ * @since 1.0.0
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/login")
+    public String get(){
+        return "/pages/login";
+    }
+}
